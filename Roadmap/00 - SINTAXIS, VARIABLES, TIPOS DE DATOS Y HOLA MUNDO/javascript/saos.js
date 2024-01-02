8381e19ecf5c437f4ec4e2d695297afffc1202ad
santi <santiirojas2000@gmail.com>
2024-01-02 17:55:43 -0300

    #00- Javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/saos.js
@@ -0,0 +1,39 @@
+EJERCICIO
+/*- Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+- Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+*/
+
+
+// LA URL DE JAVASCRIP ES https://developer.mozilla.org/es/docs/Web/JavaScript (no oficial)
+
+
+// Para crear comentarios en una sola linea
+/* PARA COMENTARIOS DE 
+MULTIPLE LINEA */
+
+// CREAR VARIABE
+
+let edad = 23;
+
+// Crear constante
+
+const nombre = 'Santiago';
+
+// TIPOS DE DATOS PRIMITIVOS
+let myNumber = 1;
+let myBool = true;
+let myString = 'Hola mundo';
+let myUndefined;
+let myNull = null;
+let mySimbol = Symbol('valor');
+
+// IMPRIMO EN LA CONSOLA
+
+console.log('Hola, Javascript');
